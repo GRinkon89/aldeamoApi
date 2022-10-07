2bd6ae0e30d1ca8a186767b07dfb28dd54003898
unknown <fricar18@hotmail.com>
2022-10-07 10:32:41 -0500

    validacion numero de pila

--- src/main/java/com/aldeamo/controller/ListasController.java
+++ src/main/java/com/aldeamo/controller/ListasController.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -20,11 +19,18 @@ public class ListasController {
 	IListasService listasService;
 	
 	@PostMapping(value = "iterar")
-	public ResponseEntity<List<Integer>> iterarListas(@RequestBody EntradaDTO entradaDto){
+	public ResponseEntity<String> iterarListas(@RequestBody EntradaDTO entradaDto){
 		List<Integer> respuesta = new ArrayList<Integer>();
 		respuesta= listasService.iterarListas(entradaDto.getIteraciones(), entradaDto.getIdPila());
-		ResponseEntity<List<Integer>> listaRespuesta = new ResponseEntity<>(respuesta, HttpStatus.OK);
-		return  listaRespuesta;		
+		if (respuesta != null) {
+			return ResponseEntity.ok(respuesta.toString());
+		}else {
+			return ResponseEntity.badRequest()
+            .body("Numero de pila no permitido, solo se permite de 1 a 5");
+		}
+		
+		
+		
 	}
 
 }
--- src/main/java/com/aldeamo/service/ListasService.java
+++ src/main/java/com/aldeamo/service/ListasService.java
@@ -12,82 +12,75 @@ import com.aldeamo.model.ArraysEntity;
 import com.aldeamo.repository.IListasRepository;
 
 @Service
-public class ListasService implements IListasService{
-	
+public class ListasService implements IListasService {
+
 	@Autowired
 	private IListasRepository listasRepository;
-	
+
 	List<ArraysEntity> listas;
 
 	@Override
 	public List<Integer> iterarListas(int iteraciones, int idPila) {
 		List<Integer> listA = new ArrayList<Integer>();
 		List<Integer> listAux = new ArrayList<Integer>();
-		listas = listasRepository.findAll();
-		
-		ArraysEntity pila = listasRepository.findById(idPila).get();
-		listA = crearLista(pila);
-		
-		System.out.println("lista de BD "  + listA);
-		
 		List<Integer> listB;
 		List<Integer> listP = Arrays.asList(2, 3, 5, 7, 11, 13, 17);
-		List<Integer> respuesta= new ArrayList<Integer>();
-		int finPila = 0;
-		int contador =0;
-		iteraciones = iteraciones-1;
-		
+		List<Integer> respuesta = new ArrayList<Integer>();
 
-		for (int i = 0; i <= iteraciones; i++) {
-			listB= new ArrayList<Integer>();
-			listB = respuesta;
-			listAux = new ArrayList<Integer>();
-			System.out.println("--------------------------" );
-			System.out.println("iteracion " + ++contador);
-			ListIterator iter = listA.listIterator(listA.size());
-			while (iter.hasPrevious()) {
-				finPila = (int) iter.previous();
-				if (finPila % listP.get(i) == 0) {
-					
-					System.out.println("numero " +finPila+"=  divisible");
-					listB.add(finPila);
-				}else {
-					System.out.println("numero " +finPila);
-					listAux.add(finPila);
-					
+		int finPila = 0;
+		int contador = 0;
+		if (idPila > 0 && idPila < 6) {
+
+			ArraysEntity pila = listasRepository.findById(idPila).get();
+			listA = crearPila(pila);
+			System.out.println("lista de BD " + listA);
+
+			iteraciones = iteraciones - 1;
+
+			for (int i = 0; i <= iteraciones; i++) {
+				listB = new ArrayList<Integer>();
+				listB = respuesta;
+				listAux = new ArrayList<Integer>();
+				System.out.println("--------------------------");
+				System.out.println("iteracion " + ++contador);
+				ListIterator iter = listA.listIterator(listA.size());
+				while (iter.hasPrevious()) {
+					finPila = (int) iter.previous();
+					if (finPila % listP.get(i) == 0) {
+
+						System.out.println("numero " + finPila + "=  divisible");
+						listB.add(finPila);
+					} else {
+						System.out.println("numero " + finPila);
+						listAux.add(finPila);
+					}
 				}
+				listA.clear();
+				listA = listAux;
+				respuesta = listB;
+
+				System.out.println("A" + contador + " " + listAux.toString());
+				System.out.println("respuesta " + respuesta.toString());
 			}
-			listA.clear();
-			listA = listAux;
-			respuesta = listB ;
-			
-			System.out.println("B "+ listB.toString());
-			System.out.println("A"+contador+" "+listAux.toString());
-			System.out.println("respuesta "+respuesta.toString());
-		}
-		if(listAux.size() > 0) {
-			for (Integer numero : listAux) {
-				respuesta.add(numero);
+			if (listAux.size() > 0) {
+				for (Integer numero : listAux) {
+					respuesta.add(numero);
+				}
+				System.out.println("respuesta final" + respuesta.toString());
 			}
-		System.out.println("respuesta final"+respuesta.toString());
+			return respuesta;
 		}
-		
-		return respuesta;
+		return null;
 	}
-	
-	private List<Integer> crearLista(ArraysEntity array){
-						
+
+	private List<Integer> crearPila(ArraysEntity array) {
+
 		List<String> pila = Arrays.asList(array.getInputArray().split(","));
-		List<Integer> pilaEnteros = new ArrayList<Integer>(); 
+		List<Integer> pilaEnteros = new ArrayList<Integer>();
 		for (String string : pila) {
-			pilaEnteros.add(Integer.parseInt(string)); 
-			
+			pilaEnteros.add(Integer.parseInt(string));
 		}
-		
-		
-		
 		return pilaEnteros;
-		
 	}
 
 }
